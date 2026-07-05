bc979f69a3e454020f10f391e5f19cf2164adb60
Paul Dreik <github@pauldreik.se>
2026-07-05 21:12:12 +0200

    implement get<T> for tuple (poorly)

--- tests/test_tuple.cpp
+++ tests/test_tuple.cpp
@@ -1,7 +1,33 @@
 #include <cassert>
 #include <tuple>
 
+namespace {
+struct S {
+  bool operator==(const S& other) const { return v == other.v; }
+  int v = 42;
+};
+} // namespace
+
 int main() {
   auto t = std::make_tuple(1, 'a', 3.14);
   assert(std::get<0>(t) == 1);
+  std::get<0>(t) = 2;
+  assert(std::get<0>(t) == 2);
+
+  for (int i = 0; i < 2; ++i) {
+    std::tuple<int, S> t2(i, S{i + 100});
+
+    assert(std::get<0>(t2) == i);
+    assert(std::get<1>(t2) == S{i + 100});
+    assert(std::get<int>(t2) == i);
+    assert(std::get<S>(t2) == S{i + 100});
+
+    const auto& t2ref = t2;
+    assert(std::get<0>(t2ref) == i);
+    assert(std::get<1>(t2ref) == S{i + 100});
+    assert(std::get<int>(t2ref) == i);
+    assert(std::get<S>(t2ref) == S{i + 100});
+
+    assert(std::get<S>(decltype(t2){}).v == 42);
+  }
 }
