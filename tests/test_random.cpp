295c6bffa59e913953d45296123f3170ffbcb194
Paul Dreik <github@pauldreik.se>
2026-07-06 23:56:25 +0200

    add parts needed to complie simdutf (except the tools)

--- tests/test_random.cpp
+++ tests/test_random.cpp
@@ -5,4 +5,16 @@ int main() {
   std::random_device rd;
   auto v = rd();
   assert(v >= rd.min());
+
+  std::mt19937 gen(1234);
+  long seed = 5678;
+  gen.seed(seed);
+
+  std::discrete_distribution<> d({1.0, 0.0, 0.0});
+  for (int i = 0; i < 10; ++i)
+    assert(d(gen) == 0);
+
+  std::uniform_int_distribution<uint16_t> full_range(0, 0xFFFF);
+  for (int i = 0; i < 1000; ++i)
+    full_range(gen);
 }
