85178a5fe78b3c5b2f28af7f66c83f15ed18a99a
Paul Dreik <github@pauldreik.se>
2026-07-12 16:19:02 +0200

    fix atomic to not be a dummy implementation

--- tests/test_atomic.cpp
+++ tests/test_atomic.cpp
@@ -1,7 +1,25 @@
 #include <atomic>
 #include <cassert>
+#include <thread>
 
 int main() {
   std::atomic<int> x{42};
   assert(x.load() == 42);
+
+  x.store(0, std::memory_order_relaxed);
+  auto increment = [&] {
+    for (int i = 0; i < 10000; ++i)
+      ++x;
+  };
+  std::thread first(increment);
+  std::thread second(increment);
+  first.join();
+  second.join();
+  assert(x.load() == 20000);
+
+  int expected = 20000;
+  assert(x.compare_exchange_strong(expected, 7));
+  assert(x.exchange(9) == 7);
+  assert(x.fetch_add(2) == 9);
+  assert(x.load() == 11);
 }
