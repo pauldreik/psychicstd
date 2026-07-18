29be63137e9309d1c68278ff5f65797fc3acd52d
Paul Dreik <github@pauldreik.se>
2026-07-18 10:25:54 +0200

    list: add get_allocator

--- tests/test_list.cpp
+++ tests/test_list.cpp
@@ -1,7 +1,17 @@
 #include "psyassert.h"
 #include <list>
 
+#include "test_allocator.h"
+
 int main() {
   std::list<int> l = {1, 2, 3};
   psyassert(l.front() == 1);
+
+  std::allocator<int> a;
+  const std::list<int> la(a);
+  psyassert(la.get_allocator() == a);
+
+  test_allocator<int> ta(1);
+  const std::list<int, test_allocator<int>> tl(ta);
+  psyassert(tl.get_allocator() == ta);
 }
