f0d45fde736ba9f39e6a79f9c92ab9d3336164a5
Paul Dreik <github@pauldreik.se>
2026-07-18 10:20:57 +0200

    type_traits: add negation_v

--- tests/test_type_traits.cpp
+++ tests/test_type_traits.cpp
@@ -1,4 +1,8 @@
 #include "psyassert.h"
 #include <type_traits>
 
-int main() { static_assert(std::is_same_v<int, int>); }
+int main() {
+  static_assert(std::is_same_v<int, int>);
+  static_assert(std::negation_v<std::false_type>);
+  static_assert(!std::negation_v<std::true_type>);
+}
