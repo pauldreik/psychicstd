ddfbecd3984579382bdb652d264dbafb4ef5b681
Paul Dreik <github@pauldreik.se>
2026-07-18 11:51:03 +0200

    type_traits: gate is_function fallback

--- tests/test_type_traits.cpp
+++ tests/test_type_traits.cpp
@@ -1,8 +1,14 @@
 #include "psyassert.h"
 #include <type_traits>
 
+using Fn = void();
+using ConstFn = void() const;
+
 int main() {
   static_assert(std::is_same_v<int, int>);
+  static_assert(std::is_function_v<Fn>);
+  static_assert(std::is_function_v<ConstFn>);
+  static_assert(!std::is_function_v<int>);
   static_assert(std::negation_v<std::false_type>);
   static_assert(!std::negation_v<std::true_type>);
 }
