fbd70c617ac3041707cea913c7c8eb431a5173e7
Paul Dreik <github@pauldreik.se>
2026-07-20 15:11:40 +0200

    add missing sqrt for float

--- tests/test_cmath.cpp
+++ tests/test_cmath.cpp
@@ -1,9 +1,11 @@
 #include "psyassert.h"
 #include <cmath>
+#include <type_traits>
 
 int main() {
   psyassert(std::fpclassify(0.0) == FP_ZERO);
   psyassert(std::fpclassify(1.0F) == FP_NORMAL);
   psyassert(std::fpclassify(HUGE_VAL) == FP_INFINITE);
   psyassert(std::fpclassify(0) == FP_ZERO);
+  static_assert(std::is_same_v<decltype(std::sqrt(1.0F)), float>);
 }
