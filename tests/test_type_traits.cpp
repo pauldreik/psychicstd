f3ea6365e141282d943267b0b97d417fd25d6dde
Paul Dreik <github@pauldreik.se>
2026-07-20 10:44:20 +0200

    use __is_pod

--- tests/test_type_traits.cpp
+++ tests/test_type_traits.cpp
@@ -7,11 +7,22 @@ struct DeletedDestructor {
   ~DeletedDestructor() = delete;
 };
 
+struct Pod {
+  int value;
+};
+
+struct NonPod {
+  NonPod() : value(0) {}
+  int value;
+};
+
 int main() {
   static_assert(std::is_same_v<int, int>);
   static_assert(std::is_same_v<const int, const int>);
   static_assert(!std::is_same_v<int, const int>);
   static_assert(!std::is_same_v<int, long>);
+  static_assert(std::is_pod_v<Pod>);
+  static_assert(!std::is_pod_v<NonPod>);
   static_assert(std::is_function_v<Fn>);
   static_assert(std::is_function_v<ConstFn>);
   static_assert(!std::is_function_v<int>);
