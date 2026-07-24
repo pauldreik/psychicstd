a5aa17e630918ccadaccf16c0ea183d1a33a3844
Paul Dreik <github@pauldreik.se>
2026-07-24 22:38:29 +0200

    inline type_info ordering

--- tests/test_typeinfo.cpp
+++ tests/test_typeinfo.cpp
@@ -1,10 +1,14 @@
 #include "psyassert.h"
 #include <string>
 #include <system_error>
+#include <typeindex>
 #include <typeinfo>
 
 int main() {
   psyassert(typeid(int) == typeid(int));
+  const std::type_index integer(typeid(int));
+  const std::type_index floating(typeid(double));
+  psyassert((integer < floating) != (floating < integer));
   psyassert(std::string(std::system_category().name()) == "system");
   psyassert(std::string(std::generic_category().name()) == "generic");
 }
