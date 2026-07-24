ffdada54945668d3ba131b9806c34dc7971f6f1d
Paul Dreik <github@pauldreik.se>
2026-07-24 22:26:49 +0200

    support member pointers in invoke_result

--- tests/test_functional.cpp
+++ tests/test_functional.cpp
@@ -8,6 +8,11 @@ template <typename T> struct holder {
 };
 holder<incomplete>* no_args() { return nullptr; }
 
+struct member_target {
+  int value = 4;
+  int add(int amount) const { return value + amount; }
+};
+
 int main() {
   auto h = std::hash<const char*>{};
   psyassert(h("hello") != 0);
@@ -17,6 +22,15 @@ int main() {
   first.swap(second);
   psyassert(first() == 2);
   psyassert(second() == 1);
+
+  member_target target;
+  psyassert(std::invoke(&member_target::value, target) == 4);
+  psyassert(std::invoke(&member_target::value, &target) == 4);
+  psyassert(std::invoke(&member_target::add, target, 3) == 7);
+  static_assert(
+      std::is_same_v<
+          std::invoke_result_t<decltype(&member_target::value), member_target&>,
+          int&>);
 
   (void)std::ref(no_args)();
 }
